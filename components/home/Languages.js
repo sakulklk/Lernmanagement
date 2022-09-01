693eabcac6dd5bf575843bdbaabd86d5a995925e
sakulklk <khms2845@gmail.com>
2022-09-01 19:13:49 +0200

    import Typography

--- components/home/Languages.js
+++ components/home/Languages.js
@@ -6,6 +6,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function Languages() {
   const loggedIn = false;
