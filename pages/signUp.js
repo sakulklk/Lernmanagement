b3cf965c3ec59c06df8b9f82d442d83fe44d139c
sakulklk <khms2845@gmail.com>
2022-09-01 19:21:58 +0200

    1 file modified, 1 deleted

--- pages/signUp.js
+++ pages/signUp.js
@@ -3,27 +3,21 @@ import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 import Login from '../components/login/loginTab.js';
 import Grid from '@mui/material/Grid';
-import Test from '../components/home/test.js';
+
 
 export default function Test() {
-  const checkboxNotTicked = true;
+
   return (
     <>
       <NavBar></NavBar>
-      {checkboxNotTicked ? (
-        <>
-          <Grid container spacing={1}>
-            <Grid item xs={6}>
-              <SignUp id="signUp"></SignUp>
-            </Grid>
-            <Grid item xs={6}>
-              <Login id="login"></Login>
-            </Grid>
-          </Grid>
-        </>
-      ) : (
-        <Test></Test>
-      )}
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <SignUp id="signUp"></SignUp>
+        </Grid>
+        <Grid item xs={6}>
+          <Login id="login"></Login>
+        </Grid>
+      </Grid>
 
       <Footer></Footer>
     </>
