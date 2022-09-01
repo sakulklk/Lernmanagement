d5060e02442be94c4e06f3b3bc7e1088433d7c84
sakulklk <khms2845@gmail.com>
2022-09-01 18:43:54 +0200

    1 file modified

--- pages/signUp.js
+++ pages/signUp.js
@@ -7,8 +7,14 @@ export default function Test() {
   return (
     <>
       <NavBar></NavBar>
+      <Grid contaner spacing ={1}>
+      <Grid item xs={3} sm={5} md={7}>
       <Login></Login>
+      </Grid>
+      <Grid item xs={9} sm={7} md={5}>
       <SignUp></SignUp>
+      </Grid>
+      </Grid>
       <Footer></Footer>
     </>
   );
