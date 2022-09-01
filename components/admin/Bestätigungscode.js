3d3b8b5dd6d4b43f78a2c4714f0cc60466ae3a19
Aleyna1234 <koca@dhbw-loerrach.de>
2022-09-01 17:04:53 +0200

    2 files modified, 1 added

--- components/admin/Bestätigungscode.js
+++ components/admin/Bestätigungscode.js
@@ -20,3 +20,5 @@ export default function Field() {
     </Box>
   );
 }
+
+//  Bestätigungscode Fenster*/
--- pages/adminrechte1.js
+++ pages/adminrechte1.js
@@ -1,24 +1,26 @@
-import NestedList from '../components/admin/Liste.js';
+
 <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/icon?family=Material+Icons"
 />;
 import NavBar from '../components/shibboleth/NavBar.js';
-import BasicTextFields from '../components/admin/Button.js';
+import BasicButtons from '../components/admin/BestätigenButton.js';
+import Field from '../components/admin/Bestätigungscode.js';
 
-export default function Test1() {  // E-mail verschicken button soll rechts von Bitte   geben sie ihre E-mail hier ein stehen 
+
+export default function Test2() {  // Funktion soll Bestätigen Button neben Bestötigungscode Feld machen
   return (
     <body>
       <NavBar></NavBar>
       <Box sx={{ p: 2 }}>
         <Grid container spacing={1}>
-          <Grid item xs={3}>
-          <BasicTextFields></BasicTextFields>
+          <Grid item xs={10}>
+          <Field></Field>  
           </Grid>
           <Grid item xs={9}>
             <Grid container spacing={1}>
               <Grid item xs={12}>
-              <NestedList></NestedList>
+              <BasicButtons></BasicButtons> 
               </Grid>
               
             </Grid>
@@ -29,5 +31,9 @@ export default function Test1() {  // E-mail verschicken button soll rechts von 
     </body>
   );
 }
+
+
+
+
 
 
