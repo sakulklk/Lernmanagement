e4859da64f640fe8e62423588bc43d14cec5de9c
sakulklk <khms2845@gmail.com>
2022-09-02 15:01:02 +0200

    2 files modified

--- components/chat/chatroom.js
+++ components/chat/chatroom.js
@@ -14,28 +14,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Fab from '@material-ui/core/Fab';
 import SendIcon from '@material-ui/icons/Send';
 
-/*const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  chatSection: {
-    width: '100%',
-    height: '80vh',
-  },
-  headBG: {
-    backgroundColor: '#e0e0e0',
-  },
-  borderRight500: {
-    borderRight: '1px solid #e0e0e0',
-  },
-  messageArea: {
-    height: '70vh',
-    overflowY: 'auto',
-  },
-});*/
-
-/*const Chat = () => {
-  const classes = useStyles();*/
 export default function Chat(){
   return (
     <div>
@@ -48,7 +26,7 @@ export default function Chat(){
       height: '80vh',
     },
     headBG: {
-      backgroundColor: '#e0e0e0',
+      backgroundColor: '#960e0e',
     },
     borderRight500: {
       borderRight: '1px solid #e0e0e0',
@@ -65,8 +43,8 @@ export default function Chat(){
           </Typography>
         </Grid>
       </Grid>
-      <Grid container component={Paper} className=/*{classes.*/'chatSection'/*}*/>
-        <Grid item xs={3} className=/*{classes.*/'borderRight500'/*}*/>
+      <Grid container component={Paper} className='chatSection'>
+        <Grid item xs={3} className='borderRight500'>
           <List>
             <ListItem button key="JessicaPrall">
               <ListItemIcon>
@@ -81,7 +59,7 @@ export default function Chat(){
           <Divider />
           <Grid item xs={12} style={{ padding: '10px' }}>
             <TextField
-              /*id="outlined-basic-email"*/
+              
               label="Search"
               variant="outlined"
               fullWidth
@@ -124,7 +102,7 @@ export default function Chat(){
           </List>
         </Grid>
         <Grid item xs={9}>
-          <List className=/*{classes.*/'messageArea'/*}*/>
+          <List className='messageArea'>
             <ListItem key="1">
               <Grid container>
                 <Grid item xs={12}>
@@ -184,7 +162,7 @@ export default function Chat(){
       </Grid>
     </div>
   );
-/*};*/
+
 }
 
-/*export default Chat;*/
+
