f9df04099b8fba345da37c1ff4a77d770b98f3d6
Aleyna1234 <110554994+Aleyna1234@users.noreply.github.com>
2022-09-03 16:15:27 +0200

    2 files modified

--- components/admin/Breadcrumb.js
+++ components/admin/Breadcrumb.js
@@ -2,28 +2,31 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
+import Linnk from 'next/link';
 
 function handleClick(event) {
   event.preventDefault();
   console.info('You clicked a breadcrumb.');
 }
 
-//deine Links funktionieren nicht!! willst du, dass man von den breadcrumbs aus wieder zu home kommt? 
+//deine Links funktionieren nicht!! willst du, dass man von den breadcrumbs aus wieder zu home kommt?
 
 export default function BasicBreadcrumbs() {
   return (
     <div role="presentation" onClick={handleClick}>
       <Breadcrumbs aria-label="breadcrumb">
-       <Link underline="hover" color="inherit" href="/">
-          Home
+        <Link underline="hover" color="inherit" href="/">
+          component={Link}
+          href= Home
         </Link>
         <Link
           underline="hover"
           color="inherit"
           // den href brauchst du nicht, der ist nur als Beispiel auf der Mui Seite gewesen villeicht hast du ihn vergessen zu ändern?
-          href="/material-ui/getting-started/installation/"
         >
-          Nutzerdaten
+          <link href="/blog">
+            <a>Nutzerdaten</a>
+          </link>
         </Link>
         <Typography color="text.primary">Adminrechte beantragen</Typography>
       </Breadcrumbs>
