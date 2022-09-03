b20ee9637c119ffe4e6cbac7cc99f8e677a67bf2
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-03 23:12:45 +0200

    1 file modified

--- components/chat/chatroom2.js
+++ components/chat/chatroom2.js
@@ -54,47 +54,121 @@ export default function Chat() {
               <ListItemText primary="Alexander Anderson"></ListItemText>
             </ListItem>
           </List>
+          <Grid item xs={3} style={{ padding: '10px' }}>
+            <TextField label="Search" variant="outlined" fullWidth />
+          </Grid>
+          <Divider />
+          <List>
+            <ListItem button key="Evan Adams">
+              <ListItemIcon>
+                <Avatar
+                  alt="Evan Adams"
+                  src="https://material-ui.com/avatar/1.jpg"
+                />
+              </ListItemIcon>
+              <ListItemText secondary="Dozent" primary="Evan Adams">
+                Evan Adams
+              </ListItemText>
+            </ListItem>
+            <ListItem button key="Josh Smith">
+              <ListItemIcon>
+                <Avatar
+                  alt="Josh Smith"
+                  src="https://material-ui.com/avatar/3.jpg"
+                />
+              </ListItemIcon>
+              <ListItemText secondary="TIF21D" primary="Josh Smith">
+                Josh Smith
+              </ListItemText>
+            </ListItem>
+            <ListItem button key="Frank Hills">
+              <ListItemIcon>
+                <Avatar
+                  alt="Frank Hills"
+                  src="https://material-ui.com/avatar/2.jpg"
+                />
+              </ListItemIcon>
+              <ListItemText secondary=" TIF21C" primary="Frank Hills">
+                Frank Hills
+              </ListItemText>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={9}>
+          <List className="messageArea">
+            <ListItem key="1">
+              <Grid container>
+                <Grid item xs={12}>
+                  <ListItemText
+                    align="right"
+                    primary="Hey man, What's up ?"
+                  ></ListItemText>
+                </Grid>
+                {/* <Grid item xs={6}>
+                  <ListItemText
+                    align="center"
+                    secondary="john"
+                    primary="prim"
+                  ></ListItemText>
+                </Grid> */}
+                <Grid item xs={12}>
+                  <ListItemText align="right" secondary="09:30"></ListItemText>
+                </Grid>
+              </Grid>
+            </ListItem>
+            <ListItem key="2">
+              <Grid container>
+                <Grid item xs={12}>
+                  <ListItemText
+                    align="left"
+                    primary="Evan Adams"
+                  ></ListItemText>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography primary="fadfa" align="left"></Typography>
+                  /*{' '}
+                  <ListItemText
+                    align="left"
+                    primary="Hey, A Good! What about you ?"
+                  ></ListItemText>{' '}
+                  */
+                </Grid>
+                <Grid item xs={12}>
+                  <ListItemText align="left" secondary="09:31"></ListItemText>
+                </Grid>
+              </Grid>
+            </ListItem>
+            <ListItem key="3">
+              <Grid container>
+                <Grid item xs={12}>
+                  <ListItemText
+                    align="right"
+                    primary="Cool. i am good, let's catch up!"
+                  ></ListItemText>
+                </Grid>
+                <Grid item xs={12}>
+                  <ListItemText align="right" secondary="10:30"></ListItemText>
+                </Grid>
+              </Grid>
+            </ListItem>
+          </List>
+          <Divider />
+          <Grid container style={{ padding: '20px' }}>
+            <Grid item xs={11}>
+              <TextField
+                id="outlined-basic-email"
+                label="Type Something"
+                fullWidth
+              />
+            </Grid>
+            <Grid xs={1} align="right">
+              <Fab color="primary" aria-label="add">
+                <SendIcon />
+              </Fab>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={3} style={{ padding: '10px' }}>
-        <TextField label="Search" variant="outlined" fullWidth />
-      </Grid>
-      <Divider />
-      <List>
-        <ListItem button key="Evan Adams">
-          <ListItemIcon>
-            <Avatar
-              alt="Evan Adams"
-              src="https://material-ui.com/avatar/1.jpg"
-            />
-          </ListItemIcon>
-          <ListItemText secondary="Dozent" primary="Evan Adams">
-            Evan Adams
-          </ListItemText>
-        </ListItem>
-        <ListItem button key="Josh Smith">
-          <ListItemIcon>
-            <Avatar
-              alt="Josh Smith"
-              src="https://material-ui.com/avatar/3.jpg"
-            />
-          </ListItemIcon>
-          <ListItemText secondary="TIF21D" primary="Josh Smith">
-            Josh Smith
-          </ListItemText>
-        </ListItem>
-        <ListItem button key="Frank Hills">
-          <ListItemIcon>
-            <Avatar
-              alt="Frank Hills"
-              src="https://material-ui.com/avatar/2.jpg"
-            />
-          </ListItemIcon>
-          <ListItemText secondary=" TIF21C" primary="Frank Hills">
-            Frank Hills
-          </ListItemText>
-        </ListItem>
-      </List>
     </div>
   );
 }
