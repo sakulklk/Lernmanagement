5d8bcf5e93a036df4c68201d795a2ff069c902e6
Aleyna1234 <110554994+Aleyna1234@users.noreply.github.com>
2022-09-03 16:46:58 +0200

    2 files modified, 1 added

--- pages/programmiersprachen.js
+++ pages/programmiersprachen.js
@@ -1,11 +1,13 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
+import Language from '../components/pogrammiersprachen/LearnProgLang.js';
 
 export default function Test() {
   return (
     <>
       <NavBar></NavBar>
       <p>Hier entsteht die Programmiersprachen-Seite</p>
+      <Language></Language>
       <Footer></Footer>
     </>
   );
