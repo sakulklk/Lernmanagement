eca02c5375d1523f6eedfc1c99af812c65c6806e
Meline-B <44995993+Meline-B@users.noreply.github.com>
2022-09-03 17:39:11 +0200

    3 files modified, 5 added forum

--- pages/post.js
+++ pages/post.js
@@ -2,13 +2,15 @@ import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 import Overview from '../components/forum/overview.js';
 import Posts from '../components/forum/posts.js';
+import Reply from '../components/forum/reply.js';
+import Question from '../components/forum/question.js';
 
 export default function Test() {
   return (
     <>
       <NavBar></NavBar>
-      <Overview> </Overview>
-      <Posts></Posts>
+      <Question> </Question>
+      <Reply> </Reply>
       <Footer></Footer>
     </>
   );
