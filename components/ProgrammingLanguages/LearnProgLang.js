db26e068319a5449535e20d2561f74d29951a67e
Aleyna1234 <110554994+Aleyna1234@users.noreply.github.com>
2022-09-05 21:29:36 +0200

    1 file modified

--- components/ProgrammingLanguages/LearnProgLang.js
+++ components/ProgrammingLanguages/LearnProgLang.js
@@ -1,5 +1,6 @@
 import ChooseLang from '../admin/ProgLanguages/LearnProgLang.js';
 
+// Programmiersprachen lernen Seite  mit angemeldetem Admin soll hier entstehen
 export default function LearnProgLangLoggedIn() {
   return <h1> Hallo </h1>;
 }
