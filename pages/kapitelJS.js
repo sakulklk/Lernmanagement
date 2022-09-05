dc92cd2b5dbdadd0ec398d89f687fd1fe31cc1a3
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-05 20:15:16 +0200

    1 file modified, 1 added, 1 deleted

--- pages/kapitelJS.js
+++ pages/kapitelJS.js
@@ -1,12 +1,10 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 
-export default function Test() {
+export default function KapitelJS() {
   return (
     <>
       <NavBar></NavBar>
-      <p>Hier entsteht die Programmiersprachen-Seite</p>
-      <a href="kapitelJS"> Test JS</a>
       <Footer></Footer>
     </>
   );
