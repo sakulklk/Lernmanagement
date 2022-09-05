7cdbe4785ca4fb8c9e4a40c9d46af831ab90cd73
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-05 20:26:54 +0200

    2 files modified

--- pages/kapitelJS.js
+++ pages/kapitelJS.js
@@ -1,10 +1,12 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
+import Übersicht from '../components/kapitelJS/kapitelübersicht.js';
 
 export default function KapitelJS() {
   return (
     <>
       <NavBar></NavBar>
+      <Übersicht></Übersicht>
       <Footer></Footer>
     </>
   );
