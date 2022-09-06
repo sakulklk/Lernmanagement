7bb147f4bbf6b5460f349b3c44a236e9ec3bbd73
Aleyna1234 <110554994+Aleyna1234@users.noreply.github.com>
2022-09-06 13:03:01 +0200

    1 file modified

--- pages/kapitelJS_na.loggedIn.js
+++ pages/kapitelJS_na.loggedIn.js
@@ -1,6 +1,6 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
-import ÜbersichtLoggedIn from '../components/kapitelJS/kapitelübersicht_na.js';
+import ÜbersichtLoggedIn from '../components/kapitelJS/kapitelübersicht_na.loggedIn.js';
 
 export default function KapitelJSloggedIn() {
   return (
