0d252e4313146aa78aa9c5d37c43070e355c7c76
Aleyna1234 <110554994+Aleyna1234@users.noreply.github.com>
2022-09-06 12:12:48 +0200

    2 files modified, 3 deleted

--- components/ProgrammingLanguages/LearnProgLang.js
+++
@@ -1,6 +0,0 @@
-import ChooseLang from '../admin/ProgLanguages/LearnProgLang.js';
-
-// Programmiersprachen lernen Seite  mit angemeldetem Admin soll hier entstehen
-export default function LearnProgLangLoggedIn() {
-  return <h1> Hallo </h1>;
-}
--- pages/programmiersprachen.js
+++ pages/programmiersprachen.js
@@ -1,16 +1,14 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
-import ChooseLang from '../components/ProgrammingLanguages/ChooseProgLang.js';
+
 // import LearnProgLangLoggedIn from '../components/ProgrammingLanguages/LearnProgLang.js';
-import VirtualizedList from '../components/ProgrammingLanguages/List.js';
 
 export default function Test() {
   return (
     <>
       <NavBar></NavBar>
       <p> Welche Programmiersprache möchtest du lernen </p>
-      <ChooseLang></ChooseLang>
-      <VirtualizedList></VirtualizedList>
+
       <Footer></Footer>
     </>
   );
