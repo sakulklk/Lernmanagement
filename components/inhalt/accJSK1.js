3a3cef2467d148a97f7947153b156b699808cfbf
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-10 20:13:07 +0200

    2 files modified

--- components/inhalt/accJSK1.js
+++ components/inhalt/accJSK1.js
@@ -1,90 +1,25 @@
-
-
-// falsch!
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
+import Typography from '@mui/material/Typography';
+import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Link from '@mui/material/Link';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const StyledMenu = styled((props) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === 'light'
-        ? 'rgb(55, 65, 81)'
-        : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        ),
-      },
-    },
-  },
-}));
-
-export default function CustomizedMenus() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+export default function Sidemenu() {
   return (
     <div>
-      {/* <Button
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        variant="contained"
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        Options
-      </Button> */}
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -94,33 +29,34 @@ export default function CustomizedMenus() {
           <Typography>Accordion 1</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <StyledMenu
-            id="demo-customized-menu"
-            MenuListProps={{
-              'aria-labelledby': 'demo-customized-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose} disableRipple>
-              <EditIcon />
-              Edit
+          {/* <Paper elevation={5}> */}
+          <MenuList>
+            <MenuItem
+              MenuItem
+              component={Link}
+              href="/nutzerdaten_kontoinformationen"
+            >
+              <Typography variant="inherit" noWrap>
+                Kontoinformationen und Statistiken
+              </Typography>
             </MenuItem>
-            <MenuItem onClick={handleClose} disableRipple>
-              <FileCopyIcon />
-              Duplicate
+
+            <MenuItem component={Link} href="/nutzerdaten_daten_aendern">
+              <Typography variant="inherit" noWrap>
+                Daten ändern
+              </Typography>
             </MenuItem>
-            <Divider sx={{ my: 0.5 }} />
-            <MenuItem onClick={handleClose} disableRipple>
-              <ArchiveIcon />
-              Archive
+            <Divider />
+            <MenuItem
+              component={Link}
+              href="/nutzerdaten_adminrechte_beantragen"
+            >
+              <Typography variant="inherit" noWrap>
+                Adminrechte beantragen
+              </Typography>
             </MenuItem>
-            <MenuItem onClick={handleClose} disableRipple>
-              <MoreHorizIcon />
-              More
-            </MenuItem>
-          </StyledMenu>
+          </MenuList>
+          {/* </Paper> */}
         </AccordionDetails>
       </Accordion>
     </div>
--- pages/inhaltJSK1.js
+++ pages/inhaltJSK1.js
@@ -1,13 +1,21 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 import SideMenu from '../components/inhalt/accJSK1.js';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 export default function Test() {
   return (
     <>
       <NavBar></NavBar>
       <p>Inahlt</p>
-      <SideMenu></SideMenu>
+      <Box sx={{ p: 2 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={4}>
+            <SideMenu></SideMenu>
+          </Grid>
+        </Grid>
+      </Box>
       <Footer></Footer>
     </>
   );
