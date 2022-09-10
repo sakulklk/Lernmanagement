040cd43d7a1ba49f96574e54a3a0917ae10be797
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-10 23:21:04 +0200

    1 file modified

--- components/inhalt/Uebersicht/inhalt.js
+++ components/inhalt/Uebersicht/inhalt.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 export default function Sidemenu() {
   return (
@@ -15,33 +16,45 @@ export default function Sidemenu() {
               margin-left: 50px;
 
             }
+            h1{
+              font-size: 36px;
+            }
+            h2{
+              font-weight: 600;
+              font-size: 32px;
+            }
             `}</style>
       <div className="content">
-        <h3>Scroll Down to See The Effect</h3>
+        <h1> JavaScript</h1>
+        <h2>Geschichte</h2>
+        <Divider />
         <p>
-          We have created a "progress bar" to{' '}
-          <b>show how far a page has been scrolled</b>.
+          JavaScript wurde 1995 in nur zwei Wochen konzipiert — daher gibt es
+          leider einige seltsame Eigenschaften.
+          <br /> <br />
+          Dennoch hat sich JavaScript als die Programmiersprache der Browser
+          durchgesetzt und kommt (neben HTML und CSS) auf fast allen Webseiten
+          zum Einsatz.
+          <br /> <br />
+          Inzwischen kommen jedes Jahr neue Spracheigenschaften hinzu, sodass
+          die oben erwähnten Nachteile heutzutage kaum noch von Bedeutung sind.
+          Durch die Ergänzung mit TypeScript und Werkzeugen wie ESLint (und
+          vielen anderen) bietet JavaScript eine moderne und robuste
+          Programmierumgebung.
         </p>
+        <h2>Vergleich mit Java</h2>
+        <Divider />
         <p>
-          It also <b>works when you scroll back up</b>.
-        </p>
-        <p>
-          It is even <b>responsive</b>! Resize the browser window to see the
-          effect.
-        </p>
-        <p>
-          Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
-          definitiones no quo, maluisset concludaturque et eum, altera fabulas
-          ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum.
-          Affert laboramus repudiandae nec et. Inciderint efficiantur his ad.
-          Eum no molestiae voluptatibus.
-        </p>
-        <p>
-          Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
-          definitiones no quo, maluisset concludaturque et eum, altera fabulas
-          ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum.
-          Affert laboramus repudiandae nec et. Inciderint efficiantur his ad.
-          Eum no molestiae voluptatibus.
+          JavaScript unterscheidet sich in vielerlei Hinsicht von Java. Siehe
+          dazu z.B. diese Tabelle.
+          <br />
+          <br />
+          Java is to JavaScript like Car is to Carpet.
+          <br />
+          <br />
+          Dennoch gibt es einige Konzepte, die in JavaScript und Java recht
+          ähnlich sind (u.a. Blöcke mit geschweifte Klammern, if/else/else if,
+          for/while).
         </p>
         <p>
           Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
