2a295801166f0e6466fe32e199457c3090959244
Aleyna1234 <110554994+Aleyna1234@users.noreply.github.com>
2022-09-12 19:06:57 +0200

    3 files modified, 2 added

--- components/admin/Breadcrumb.js
+++ components/admin/Breadcrumb.js
@@ -72,22 +72,3 @@ function handleClick(event) {
   console.info('You clicked a breadcrumb.');
 }
 
-export default function BasicBreadcrumbs1() {
-  return (
-    <div role="presentation" onClick={handleClick}>
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="/">
-          <a href="/"> Home</a>
-        </Link>
-        <Link
-          underline="hover"
-          color="inherit"
-          href="/material-ui/getting-started/installation/"
-        >
-          <a href="nutzerdaten_kontoinformationen"> Nutzerdaten</a>
-        </Link>
-        <Typography color="text.primary">Breadcrumbs</Typography>
-      </Breadcrumbs>
-    </div>
-  );
-}
---
+++ pages/Breadcrumb1.js
@@ -0,0 +1,19 @@
+export default function BasicBreadcrumbs1() {
+  return (
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          <a href="/"> Home</a>
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+        >
+          <a href="pages/nutzerdaten_kontoinformationen"> Nutzerdaten</a>
+        </Link>
+        <Typography color="text.primary">Breadcrumbs</Typography>
+      </Breadcrumbs>
+    </div>
+  );
+}
