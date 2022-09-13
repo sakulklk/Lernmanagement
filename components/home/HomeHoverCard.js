698c80bcf1ae7f3b8ce55636243369c19d55756a
JessiByte <110539125+JessiByte@users.noreply.github.com>
2022-09-13 02:01:11 +0200

    1 file modified, 3 added, 1 deleted

---
+++ components/home/HomeHoverCard.js
@@ -0,0 +1,164 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+
+export default function HomeHoverCard(text) {
+  return (
+    <Paper
+      className="hoverWrapper"
+      elevation={5}
+      component={Link}
+      href={text.href}
+      sx={{
+        textDecoration: 'none',
+        width: '100%',
+        height: '100px',
+        display: 'block',
+
+        background: '#1565c0',
+        margin: '10px',
+        textAlign: 'center',
+        color: 'white',
+        '&:hover': {
+          paddingTop: '5px',
+          opacity: '0.8',
+          '& .description': {
+            display: 'block',
+          },
+        },
+      }}
+    >
+      <Typography className="title" variant="h6" style={{ paddingTop: '13px' }}>
+        {text.title}
+      </Typography>
+      <Typography
+        className="description"
+        variant="subtitle2"
+        display="none"
+        alignSelf="end"
+      >
+        {text.description}
+      </Typography>
+      {text.icon}
+    </Paper>
+  );
+}
+
+// const images = [
+//   {
+//     title: 'Breakfast',
+//     width: '30%',
+//   },
+//   {
+//     title: 'Burgers',
+//     width: '40%',
+//   },
+//   {
+//     title: 'Camera',
+//     width: '30%',
+//   },
+// ];
+
+// const ImageButton = styled(ButtonBase)(({ theme }) => ({
+//   position: 'relative',
+//   height: 200,
+//   [theme.breakpoints.down('sm')]: {
+//     width: '100% !important', // Overrides inline-style
+//     height: 100,
+//   },
+//   '&:hover, &.Mui-focusVisible': {
+//     zIndex: 1,
+//     '& .MuiImageBackdrop-root': {
+//       opacity: 0.15,
+//     },
+//     '& .MuiImageMarked-root': {
+//       opacity: 0,
+//     },
+//     '& .MuiTypography-root': {
+//       border: '4px solid yellow',
+//     },
+//   },
+// }));
+
+// const ImageSrc = styled('span')({
+//   position: 'absolute',
+//   left: 0,
+//   right: 0,
+//   top: 0,
+//   bottom: 0,
+//   backgroundSize: 'cover',
+//   backgroundPosition: 'center 40%',
+// });
+
+// const Image = styled('span')(({ theme }) => ({
+//   position: 'absolute',
+//   left: 0,
+//   right: 0,
+//   top: 0,
+//   bottom: 0,
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   color: theme.palette.common.white,
+// }));
+
+// const ImageBackdrop = styled('span')(({ theme }) => ({
+//   position: 'absolute',
+//   left: 0,
+//   right: 0,
+//   top: 0,
+//   bottom: 0,
+//   backgroundColor: theme.palette.common.black,
+//   opacity: 0.4,
+//   transition: theme.transitions.create('opacity'),
+// }));
+
+// const ImageMarked = styled('span')(({ theme }) => ({
+//   height: 3,
+//   width: 18,
+//   backgroundColor: theme.palette.common.white,
+//   position: 'absolute',
+//   bottom: -2,
+//   left: 'calc(50% - 9px)',
+//   transition: theme.transitions.create('opacity'),
+// }));
+
+// export default function ButtonBases() {
+//   return (
+//     <Box
+//       sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}
+//     >
+//       {images.map((image) => (
+//         <ImageButton
+//           focusRipple
+//           key={image.title}
+//           style={{
+//             width: image.width,
+//           }}
+//         >
+//           <ImageBackdrop className="MuiImageBackdrop-root" />
+//           <Image>
+//             <Typography
+//               component="span"
+//               variant="subtitle1"
+//               color="inherit"
+//               sx={{
+//                 position: 'relative',
+//                 p: 1,
+//                 pt: 0.1,
+//                 pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+//               }}
+//             >
+//               {image.title}
+//               <ImageMarked className="MuiImageMarked-root" />
+//             </Typography>
+//           </Image>
+//         </ImageButton>
+//       ))}
+//     </Box>
+//   );
+// }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Bild from '../components/home/Bild.js';
 import LanguageWidget from '../components/home/LanguageWidget.js';
 import Languages from '../components/home/Languages.js';
 import Footer from '../components/Footer.js';
-import Dashboard from '../components/home/Dashboard.js';
-
+import ForumQuestion from '../components/home/ForumQuestion.js';
+import HomeHoverCard from '../components/home/HomeHoverCard.js';
+import Grid from '@mui/material/Grid';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import StarPurple500Icon from '@mui/icons-material/StarPurple500';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 export default function Home() {
   const loggedIn = true;
   return (
@@ -15,7 +19,34 @@ export default function Home() {
         {loggedIn ? (
           <>
             <LanguageWidget></LanguageWidget>
-            <Dashboard></Dashboard>
+            <Grid container spacing={3}>
+              <Grid item xs>
+                <HomeHoverCard
+                  title="Zufällige Übungen"
+                  description="Übe hier zu allen Themengebieten, die du bisher gelernt hast"
+                  icon={<ShuffleIcon />}
+                  href="/home_dashboard_random_challenge"
+                ></HomeHoverCard>
+              </Grid>
+              <Grid item xs>
+                <HomeHoverCard
+                  title="Daily"
+                  description="Bearbeite die tägliche Nussknacker-Aufgabe und sammle Punkte"
+                  icon={<StarPurple500Icon />}
+                  href="/home_dashboard_daily"
+                ></HomeHoverCard>
+              </Grid>
+              <Grid item xs>
+                <HomeHoverCard
+                  title="Challenges"
+                  description="Wage dich an eine der drei Challenge-Kategorien und sammle Punkte"
+                  icon={<FitnessCenterIcon />}
+                  href="/home_dashboard_challenge_categories"
+                ></HomeHoverCard>
+              </Grid>
+            </Grid>
+
+            <ForumQuestion></ForumQuestion>
           </>
         ) : (
           <Languages></Languages>
