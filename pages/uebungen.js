0635896678117bffae520a0aef61f341dbf9695b
Shadolanc <112266932+Shadolanc@users.noreply.github.com>
2022-09-15 13:39:49 +0200

    1 file modified, 2 added

--- pages/uebungen.js
+++ pages/uebungen.js
@@ -6,6 +6,7 @@ import MultipleChoice from '../components/uebungen/MultipleChoice.js';
 import Crosswords from '../components/uebungen/Crosswords.js';
 import DragDrop from '../components/uebungen/DragDrop.js';
 import DragDrop2 from '../components/uebungen/DragDrop2.js';
+import GapText from '../components/uebungen/GapText.js';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 //npm install react-dnd react-dnd-html5-backend --save
@@ -56,6 +57,16 @@ export default function Uebungen() {
         DragDrop2
       </Button>
 
+      <Button
+        style={{ padding: '30px', margin: '10px' }}
+        variant="outlined"
+        onClick={() => {
+          setType('GapText');
+        }}
+      >
+        GapText
+      </Button>
+
       {chosenType == 'MultipleChoice' ? (
         <MultipleChoice></MultipleChoice>
       ) : (
@@ -63,6 +74,7 @@ export default function Uebungen() {
       )}
       {chosenType == 'Crosswords' ? <Crosswords></Crosswords> : <></>}
       {chosenType == 'DragDrop' ? <DragDrop></DragDrop> : <></>}
+      {chosenType == 'GapText' ? <GapText></GapText> : <></>}
       {chosenType == 'DragDrop2' ? (
         //<DndProvider>
         <DragDrop2></DragDrop2>
