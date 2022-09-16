36a40d8754a00c0f104ba2494c51c745cbce3e87
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-16 15:29:24 +0200

    Update programmiersprachen_uebersicht.js

--- pages/programmiersprachen_uebersicht.js
+++ pages/programmiersprachen_uebersicht.js
@@ -1,37 +1,147 @@
+Skip to content
+Search or jump to…
+Pulls
+Issues
+Marketplace
+Explore
+ 
+@sakulklk 
+sakulklk
+/
+Lernmanagement
+Public
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Lernmanagement/pages/programmiersprachen_uebersicht.js /
+@JessiByte
+JessiByte 3 files modified, 3 added
+Latest commit 49620fa 3 days ago
+ History
+ 1 contributor
+103 lines (101 sloc)  2.68 KB
+
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
-// import BreadcrumbPs from '../components/admin/breadcrumb2.js';
-import * as React from 'react';
+import LernsprachenPaper from '../components/lernsprachen/LernsprachenPaper.js';
+import LernsprachenKategorie from '../components/lernsprachen/LernsprachenKategorie.js';
+import Grid from '@mui/material/Grid';
+
 import Typography from '@mui/material/Typography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
 
 export default function Test() {
+  const Dozent = true;
+  const learningLanguagesCategories = [
+    'Webentwicklung',
+    'DataScience',
+    'Datenbanken',
+    'Andere',
+  ];
+  const learningLanguages = [
+    {
+      name: 'JavaScript',
+      progress: 80,
+      learningMark: 'JSON',
+      category: 'Webentwicklung',
+    },
+    {
+      name: 'Python',
+      progress: 0,
+      learningMark: null,
+      category: 'DataScience',
+    },
+    {
+      name: 'HTML',
+      progress: 0,
+      learningMark: null,
+      category: 'Webentwicklung',
+    },
+    {
+      name: 'CSS',
+      progress: 77,
+      learningMark: 'Animationen',
+      category: 'Webentwicklung',
+    },
+    {
+      name: 'PHP',
+      progress: 0,
+      learningMark: null,
+      category: 'Datenbanken',
+    },
+    {
+      name: 'C++',
+      progress: 0,
+      learningMark: null,
+      category: 'Datenbanken',
+    },
+    {
+      name: 'Prolog',
+      progress: 55,
+      learningMark: 'Arithmetik',
+      category: 'Andere',
+    },
+  ];
   return (
     <>
-      <NavBar></NavBar>
-
-    
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="/">
-          Home
-        </Link>
-        <Link underline="hover" color="inherit" href="programmiersprachen.js">
-          Programmiersprachen
-        </Link>
-      </Breadcrumbs>
-
-      <p>Hier entsteht die Programmiersprachen-Seite</p>
-      <a href="kapitelJS_na"> Test JS</a>
-      <br />
-      <a href="kapitelJSD"> Test JS Dozent</a>
-      <br />
-
-      <a href="hello_world"> Test Inhalt</a>
-      <br />
-      <a href="uebersichtJS"> Test Übersicht</a>
-
+      <div style={{ marginBottom: '65px' }}>
+        <NavBar></NavBar>
+        <Grid container spacing={1}>
+          {learningLanguagesCategories.map((learningLanguageCategory) => (
+            <Grid
+              item
+              xs={12}
+              style={{
+                margin: '30px',
+                marginBottom: '10px',
+                padding: '0px',
+              }}
+            >
+              <LernsprachenKategorie name={learningLanguageCategory} />
+              {learningLanguages.map((learningLanguage) =>
+                learningLanguage.category == learningLanguageCategory ? (
+                  <Grid
+                    item
+                    xs={12}
+                    style={{
+                      margin: '30px',
+                      padding: '0px',
+                    }}
+                  >
+                    {' '}
+                    <LernsprachenPaper
+                      name={learningLanguage.name}
+                      progress={learningLanguage.progress}
+                      learningMark={learningLanguage.learningMark}
+                    />
+                  </Grid>
+                ) : null
+              )}
+            </Grid>
+          ))}
+        </Grid>
+      </div>
       <Footer></Footer>
     </>
   );
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
