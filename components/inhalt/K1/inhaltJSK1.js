334e1241aa72c08638c477b07fac6b9e93043344
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-16 14:47:26 +0200

    1 file modified

--- components/inhalt/K1/inhaltJSK1.js
+++ components/inhalt/K1/inhaltJSK1.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 
-import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
 
