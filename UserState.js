10ffe253746e73f51ecf648b5a3135a993f4f01a
JessiByte <110539125+JessiByte@users.noreply.github.com>
2022-09-27 10:26:16 +0200

    1 file modified, 2 added, 1 deleted

--- UserState.js
+++
@@ -1,3 +0,0 @@
-export default function UserState(userState) {
-  return { userState };
-}
--- pages/chat.js
+++ pages/chat.js
@@ -2,9 +2,11 @@ import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 import Chat from '../components/chat/chatTab.js';
 import Grid from '@mui/material/Grid';
+import { userState, userStateOptions } from '../userState.js';
 
 export default function Test() {
-  console.log(globalVariable);
+  userState = userStateOptions[1];
+
   return (
     <>
       <NavBar></NavBar>
