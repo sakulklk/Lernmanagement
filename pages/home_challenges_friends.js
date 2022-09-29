aaf046c805a92d0b0563022adba42ec8f8dd33db
JessiByte <110539125+JessiByte@users.noreply.github.com>
2022-09-29 13:01:05 +0200

    3 files modified

--- pages/home_challenges_friends.js
+++ pages/home_challenges_friends.js
@@ -1,13 +1,34 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 export default function Test() {
   return (
     <>
-          <div style={{ paddingBottom: '65px' }}>
-
-      <NavBar status="logged" navOption="Nutzerdaten"></NavBar>
+      <div style={{ paddingBottom: '65px' }}>
+        <NavBar status="logged" navOption="Nutzerdaten"></NavBar>
+        <Paper
+          elevation={5}
+          style={{
+            margin: '50px auto',
+            padding: '20px',
+            width: '80%',
+            textAlign: 'center',
+            color: 'green',
+          }}
+        >
+          <Typography variant="h5">
+            Hier soll eine "against friends"-Challenge implementiert werden:
+            Zwei Nutzer können gegeneinander antreten und jeweils die selben
+            (willkürlich gewählten) Übungen bearbeiten. Um diese Challenge
+            antreten zu können, müssen beide User mindestens zwei gleiche
+            Unterkapitel (können aus unterschiedlichen Lernkategorien kommen)
+            bearbeitet haben. Für die Bearbeitug der Aufgaben gibt es eine
+            Maximalzeitspanne, welche beim Erstellen von den Dozenten bestimmt
+            wird.
+          </Typography>
+        </Paper>
       </div>
       <Footer></Footer>
     </>
