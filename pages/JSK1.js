78666473db7fc3aee140979261e1f559a444bb0e
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-29 16:34:40 +0200

    1 file modified

--- pages/JSK1.js
+++ pages/JSK1.js
@@ -12,9 +12,8 @@ import SideMenuRight from '../components/inhalt/sideMenuRight.js';
 export default function Test() {
   return (
     <>
-
       <NavBar status="logged" navOption="Nutzerdaten"></NavBar>
-      <div style={{ paddingBottom: '65px' paddingTop: "400px" }}>
+      <div style={{ paddingBottom: '65px', paddingTop: '400px' }}>
         <Grid container spacing={1}>
           <Grid item xs={2}>
             <Box sx={{ p: 2 }}>
