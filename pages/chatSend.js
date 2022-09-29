940cd7571588cfe6465cf9a516f08bfb900b8411
sakulklk <100590062+sakulklk@users.noreply.github.com>
2022-09-29 16:33:04 +0200

    1 file modified

--- pages/chatSend.js
+++ pages/chatSend.js
@@ -1,15 +1,29 @@
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
-
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 export default function Test() {
   return (
     <>
-          <div style={{ paddingBottom: '65px' }}>
-
-    <NavBar></NavBar>
-      <p> Nach dem Drücken auf das SendIcon sollte Nachricht gesendet werden. Außerdem muss ebenfalls das switchen unter den verschiedenen Chats ermöglicht werden. </p> 
-      
+      <div style={{ paddingBottom: '65px' }}>
+        <NavBar status="logged" navOption="Nutzerdaten"></NavBar>
+        <Paper
+          elevation={5}
+          style={{
+            margin: '50px auto',
+            padding: '20px',
+            width: '80%',
+            textAlign: 'center',
+            color: 'green',
+          }}
+        >
+          <Typography variant="h5">
+            In Zukunft muss für den Chat eine Sende Funktion implementiert
+            werden. Außerdem sollte man zwischen den verschiedenen Chats
+            switchen können.{' '}
+          </Typography>
+        </Paper>
       </div>
 
       <Footer></Footer>
